eb0b64cb6518e1c64b16bf3e5d710b717687f35d
Dominik Dorfmeister <office@dorfmeister.cc>
2020-08-09 14:38:47 +0200

    add path prefix to serve under /blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ require(`dotenv`).config({
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
+    pathPrefix: `/blog`,
     siteMetadata: {
         siteTitleAlt: `Minimal Blog - Gatsby Theme`,
     },
