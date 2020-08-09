8f499e268e56dc9b17cce2d88b0d87d16ef1fd03
Dominik Dorfmeister <office@dorfmeister.cc>
2020-08-09 15:47:21 +0200

    fill Hero page with some basics

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/texts/About.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+const About = () => (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+        <img
+            src="/profile-pic.jpg"
+            alt="TkDodo"
+            style={{ marginRight: '0.875rem', width: '4rem', height: '4rem', borderRadius: '50%' }}
+        />
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div>Personal blog by TkDodo</div>
+            <div>Opinions are my own</div>
+        </div>
+    </div>
+)
+
+export default About
