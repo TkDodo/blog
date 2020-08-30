fedbf42050ecdf0c30475dd61a2abd75c3bbd078
Dominik Dorfmeister <office@dorfmeister.cc>
2020-08-30 22:01:33 +0200

    make dark/light theme of the comments section dependent on the actual darkmode toggle

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
+import { useColorMode } from 'theme-ui'
 
 const id = 'inject-comments'
 
 const Comments = () => {
+    const [colorMode] = useColorMode()
+
     React.useEffect(() => {
         const script = document.createElement('script')
         const parent = document.getElementById(id)
@@ -10,11 +13,17 @@ const Comments = () => {
         script.setAttribute('src', 'https://utteranc.es/client.js')
         script.setAttribute('repo', 'TkDodo/blog-comments')
         script.setAttribute('issue-term', 'pathname')
-        script.setAttribute('theme', 'photon-dark')
+        script.setAttribute('theme', colorMode === 'dark' ? 'github-dark' : 'github-light')
         script.setAttribute('crossorigin', 'anonymous')
         script.setAttribute('async', 'true')
         parent.appendChild(script)
-    }, [])
+
+        return () => {
+            while (parent.firstChild) {
+                parent.removeChild(parent.lastChild)
+            }
+        }
+    }, [colorMode])
 
     return <div id={id} />
 }
