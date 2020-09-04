8a226d2813a26f711352df8f2ffb05d2ff39b852
Dominik Dorfmeister <office@dorfmeister.cc>
2020-09-04 21:54:24 +0200

    add SpacingBox
    use case: codeBlocks directly under headings looks weird, we need more spacing there

---
+++ src/components/SpacingBox.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Box } from '@theme-ui/components'
+
+const SpacingBox = () => (
+    <Box as="p" sx={{ mb: 3 }}>
+        {' '}
+    </Box>
+)
+
+export default SpacingBox
