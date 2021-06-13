165b7295df11524ed8591dc9a41bace9ef1b1de5
Dominik Dorfmeister <office@dorfmeister.cc>
2021-06-13 12:05:59 +0200

    useColorSchemeMediaQuery 🚀
    so that we can respect the preferes-color-scheme media query

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,20 +1,21 @@
 import baseTheme from '@lekoarts/gatsby-theme-minimal-blog/src/gatsby-plugin-theme-ui'
 
 export default {
-    ...baseTheme,
-    colors: {
-        ...baseTheme.colors,
-        primary: '#a10f15',
-        modes: {
-            ...baseTheme.colors.modes,
-            light: {
-                ...baseTheme.colors.modes.light,
-                background: '#F5F5F5',
-            },
-            dark: {
-                ...baseTheme.colors.modes.dark,
-                primary: '#f0666b',
-            },
-        },
+  ...baseTheme,
+  useColorSchemeMediaQuery: true,
+  colors: {
+    ...baseTheme.colors,
+    primary: '#a10f15',
+    modes: {
+      ...baseTheme.colors.modes,
+      light: {
+        ...baseTheme.colors.modes.light,
+        background: '#F5F5F5',
+      },
+      dark: {
+        ...baseTheme.colors.modes.dark,
+        primary: '#f0666b',
+      },
     },
+  },
 }
