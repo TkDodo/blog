3dae8c4af3815650f508163b3855434c75c8c4fe
Dominik Dorfmeister <office@dorfmeister.cc>
2021-06-20 12:08:40 +0200

    prettify root dir

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 exports.onCreateWebpackConfig = ({ actions }) => {
-    actions.setWebpackConfig({
-        resolve: {
-            modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-        },
-    })
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+  })
 }
