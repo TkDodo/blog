fa743c4f919d332d3ed2e372ba97d0ded9e32f9d
Dominik Dorfmeister <office@dorfmeister.cc>
2021-06-20 12:04:09 +0200

    prettify

--- src/components/Attribution.tsx
+++ src/components/Attribution.tsx
@@ -2,28 +2,28 @@ import * as React from 'react'
 import { Box, Divider, Link } from '@theme-ui/components'
 
 type Props = {
-    name: string
-    url: string
-    prefix?: string
+  name: string
+  url: string
+  prefix?: string
 }
 
 const Attribution = ({ name, url, prefix = 'Photo by' }: Props) => (
-    <>
-        <Box
-            sx={{
-                fontSize: 'smaller',
-                textAlign: 'center',
-                marginTop: ['-1rem', '-1rem', '-3rem'],
-                marginBottom: '1rem',
-            }}
-        >
-            {prefix}{' '}
-            <Link href={url} target="_blank" rel="noreferrer noopener">
-                {name}
-            </Link>
-        </Box>
-        <Divider />
-    </>
+  <>
+    <Box
+      sx={{
+        fontSize: 'smaller',
+        textAlign: 'center',
+        marginTop: ['-1rem', '-1rem', '-3rem'],
+        marginBottom: '1rem',
+      }}
+    >
+      {prefix}{' '}
+      <Link href={url} target="_blank" rel="noreferrer noopener">
+        {name}
+      </Link>
+    </Box>
+    <Divider />
+  </>
 )
 
 export default Attribution
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -4,39 +4,43 @@ import { useColorMode, Box, Divider } from 'theme-ui'
 const id = 'inject-comments'
 
 type Props = {
-    children?: React.ReactNode
-    withSeparator: boolean
+  children?: React.ReactNode
+  withSeparator: boolean
 }
 
 const Comments = ({ children, withSeparator = true }: Props) => {
-    const [colorMode] = useColorMode()
+  const [colorMode] = useColorMode()
 
-    React.useEffect(() => {
-        const script = document.createElement('script')
-        const parent = document.getElementById(id)
+  React.useEffect(() => {
+    const script = document.createElement('script')
+    const parent = document.getElementById(id)
 
-        script.setAttribute('src', 'https://utteranc.es/client.js')
-        script.setAttribute('repo', 'TkDodo/blog-comments')
-        script.setAttribute('issue-term', 'pathname')
-        script.setAttribute('theme', colorMode === 'dark' ? 'github-dark' : 'github-light')
-        script.setAttribute('crossorigin', 'anonymous')
-        script.setAttribute('async', 'true')
-        parent.appendChild(script)
-
-        return () => {
-            while (parent.firstChild) {
-                parent.removeChild(parent.lastChild)
-            }
-        }
-    }, [colorMode])
-
-    return (
-        <>
-            {withSeparator && <Divider />}
-            {children && <Box sx={{ fontSize: [1, 1, 2] }}>{children}</Box>}
-            <div id={id} />
-        </>
+    script.setAttribute('src', 'https://utteranc.es/client.js')
+    script.setAttribute('repo', 'TkDodo/blog-comments')
+    script.setAttribute('issue-term', 'pathname')
+    script.setAttribute(
+      'theme',
+      colorMode === 'dark' ? 'github-dark' : 'github-light'
     )
+
+    script.setAttribute('crossorigin', 'anonymous')
+    script.setAttribute('async', 'true')
+    parent.appendChild(script)
+
+    return () => {
+      while (parent.firstChild) {
+        parent.removeChild(parent.lastChild)
+      }
+    }
+  }, [colorMode])
+
+  return (
+    <>
+      {withSeparator && <Divider />}
+      {children && <Box sx={{ fontSize: [1, 1, 2] }}>{children}</Box>}
+      <div id={id} />
+    </>
+  )
 }
 
 export default Comments
