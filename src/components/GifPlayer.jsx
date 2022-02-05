540c2439036c1abe1308d2251784c92bb860e167
Dominik Dorfmeister <office@dorfmeister.cc>
2022-02-05 22:04:06 +0100

    okay, withPrefix

--- src/components/GifPlayer.jsx
+++ src/components/GifPlayer.jsx
@@ -1,6 +1,13 @@
 import Loadable from '@loadable/component'
 import 'react-gif-player/dist/gifplayer.css'
+import { withPrefix } from 'gatsby'
 
 const GifPlayer = Loadable(() => import('react-gif-player'))
 
-export default GifPlayer
+export default ({ gif, still, ...props }) => (
+  <GifPlayer
+    {...props}
+    gif={`${withPrefix(gif)}`}
+    still={`${withPrefix(still)}`}
+  />
+)
