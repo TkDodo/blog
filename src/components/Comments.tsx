bedfb39841d4f25b45a2931d911aca0b787167bc
Dominik Dorfmeister <office@dorfmeister.cc>
2022-04-15 18:12:47 +0200

    remove children from comments
    because spacing is better that way

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -4,31 +4,24 @@ import { useColorMode, Box } from 'theme-ui'
 
 const id = 'inject-comments'
 
-type Props = {
-  children?: React.ReactNode
-}
-
-const Comments = ({ children }: Props) => {
+const Comments = () => {
   const [colorMode] = useColorMode()
 
   return (
-    <>
-      {children && <Box sx={{ fontSize: [1, 1, 2] }}>{children}</Box>}
-      <Giscus
-        id={id}
-        repo="tkdodo/blog-comments"
-        repoId="MDEwOlJlcG9zaXRvcnkyOTE1MzI1NjI="
-        category="Announcements"
-        categoryId="DIC_kwDOEWBvEs4COl22"
-        mapping="pathname"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        inputPosition="top"
-        theme={colorMode === 'dark' ? 'dark' : 'light'}
-        lang="en"
-        loading="lazy"
-      />
-    </>
+    <Giscus
+      id={id}
+      repo="tkdodo/blog-comments"
+      repoId="MDEwOlJlcG9zaXRvcnkyOTE1MzI1NjI="
+      category="Announcements"
+      categoryId="DIC_kwDOEWBvEs4COl22"
+      mapping="pathname"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme={colorMode === 'dark' ? 'dark' : 'light'}
+      lang="en"
+      loading="lazy"
+    />
   )
 }
 
