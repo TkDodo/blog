7015e83453c6947b1962dc4fdb7f38e10e1bbab0
Dominik Dorfmeister <office@dorfmeister.cc>
2022-04-15 17:53:28 +0200

    Revert "try the manual version"
    This reverts commit 29f35572dc49cc3191a7c24e6f54986aac807efa.

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import Giscus from '@giscus/react'
 import { useColorMode, Box } from 'theme-ui'
 
 const id = 'inject-comments'
@@ -10,42 +11,23 @@ type Props = {
 const Comments = ({ children }: Props) => {
   const [colorMode] = useColorMode()
 
-  React.useEffect(() => {
-    const script = document.createElement('script')
-    const parent = document.getElementById(id)
-
-    script.setAttribute('src', 'https://giscus.app/client.js')
-    script.setAttribute('data-repo', 'tkdodo/blog-comments')
-    script.setAttribute('data-repo-id', 'MDEwOlJlcG9zaXRvcnkyOTE1MzI1NjI=')
-    script.setAttribute('data-category', 'Announcements')
-    script.setAttribute('data-category-id', 'DIC_kwDOEWBvEs4COl22')
-    script.setAttribute('data-mapping', 'pathname')
-    script.setAttribute('data-reactions-enabled', '1')
-    script.setAttribute('data-emit-metadata', '0')
-    script.setAttribute('data-input-position', 'top')
-    script.setAttribute('data-lang', 'en')
-    script.setAttribute('data-loading', 'lazy')
-
-    script.setAttribute(
-      'data-theme',
-      colorMode === 'dark' ? 'github-dark' : 'github-light'
-    )
-
-    script.setAttribute('crossorigin', 'anonymous')
-    script.setAttribute('async', 'true')
-    parent.appendChild(script)
-
-    return () => {
-      while (parent.firstChild) {
-        parent.removeChild(parent.lastChild)
-      }
-    }
-  }, [colorMode])
-
   return (
     <>
       {children && <Box sx={{ fontSize: [1, 1, 2] }}>{children}</Box>}
-      <div id={id} />
+      <Giscus
+        id={id}
+        repo="tkdodo/blog-comments"
+        repoId="MDEwOlJlcG9zaXRvcnkyOTE1MzI1NjI="
+        category="Announcements"
+        categoryId="DIC_kwDOEWBvEs4COl22"
+        mapping="pathname"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme={colorMode === 'dark' ? 'github-dark' : 'github-light'}
+        lang="en"
+        loading="lazy"
+      />
     </>
   )
 }
