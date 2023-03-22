8a7b49c5bd2e0156197b50eb3f85a38756226117
Dominik Dorfmeister <office@dorfmeister.cc>
2023-03-22 10:34:35 +0100

    link to monolisa

--- src/components/MonoLisa.tsx
+++ src/components/MonoLisa.tsx
@@ -5,13 +5,9 @@ const MonoLisa = () => {
   return (
     <Box css={{ fontFamily: 'ml, monospace', textAlign: 'center' }}>
       <Box>Like the monospace font in the code blocks?</Box>
-
       <Box>
         Check out{' '}
-        <Link
-          target="_blank"
-          href="https://a.paddle.com/v2/click/105822/165267?link=2447"
-        >
+        <Link target="_blank" href="https://www.monolisa.dev/">
           monolisa.dev
         </Link>
       </Box>
