4cec2cc1a043fa3f14504f7200db7761694a8ced
Dominik Dorfmeister <office@dorfmeister.cc>
2023-07-22 14:33:37 +0200

    sponsors update

--- src/components/thinking-in-react-query/slide.tsx
+++ src/components/thinking-in-react-query/slide.tsx
@@ -3,14 +3,6 @@ import * as React from 'react'
 
 import { Flex, Box, Text, Divider } from '@theme-ui/components'
 
-/*<img
-      width="100%"
-      src={withPrefix(
-        `images/thinking-in-react-query/${index + 1}.png`
-      )}
-      alt={`Slide ${index + 1}`}
-    />*/
-
 export const Slide = ({ children, index }) => (
   <Flex
     sx={{
