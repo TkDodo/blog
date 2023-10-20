5fa389e0e3067a0774e697de92322b8784d26352
Dominik Dorfmeister <office@dorfmeister.cc>
2023-10-20 22:23:05 +0200

    aside icon

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,13 +1,36 @@
 import Emph from './Emph'
 import * as React from 'react'
-import { Box } from 'theme-ui'
+import { Box, Flex } from 'theme-ui'
 
 type Props = {
   children: React.ReactNode
   title?: string
+  icon?: keyof typeof icons
 }
 
-const Aside = ({ children, title }: Props) => {
+const Bell = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+    <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+  </svg>
+)
+
+const icons: Record<string, React.ComponentType> = {
+  bell: Bell,
+}
+
+const Aside = ({ children, title, icon }: Props) => {
+  const Icon = icon ? icons[icon] : null
   return (
     <Box
       as="aside"
@@ -28,7 +51,18 @@ const Aside = ({ children, title }: Props) => {
         },
       }}
     >
-      {title && <Emph>{title}</Emph>}
+      {title || icon ? (
+        <Flex
+          sx={{
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            color: 'var(--theme-ui-colors-primary)',
+          }}
+        >
+          {title && <Emph>{title}</Emph>}
+          {icon && <Icon />}
+        </Flex>
+      ) : null}
       {children}
     </Box>
   )
