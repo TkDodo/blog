b367ba985cebed10c175bc40826428949e0c21f7
Dominik Dorfmeister <office@dorfmeister.cc>
2023-11-16 11:03:39 +0100

    feat: generate og-images

---
+++ src/components/OgImage.tsx
@@ -0,0 +1,94 @@
+import * as React from 'react'
+
+type Props = {
+  title: string
+  img: {
+    src: string
+    width: number
+    height: number
+  }
+}
+
+const OgImage = ({ title, img }: Props) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        padding: 48,
+        height: '100%',
+        backgroundColor: '#2e3440',
+      }}
+    >
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'space-between',
+          flexDirection: 'column',
+          // backgroundColor: 'white',
+          color: '#cbd5e0',
+          // padding: 48,
+          // borderRadius: 12,
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: 16,
+            flexGrow: 1,
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              gap: 16,
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                fontSize: 64,
+                maxWidth: 600,
+                width: 600,
+                fontWeight: 600,
+              }}
+            >
+              {title}
+            </div>
+            <div
+              style={{
+                fontSize: 36,
+                fontWeight: 400,
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <img
+                src="https://avatars.githubusercontent.com/u/1021430?v=4"
+                alt="TkDodo"
+                width={120}
+                height={120}
+                style={{ borderRadius: '50%', marginRight: 16 }}
+              />
+              by TkDodo
+            </div>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <img {...img} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default OgImage
