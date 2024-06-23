6e2c0c80e9bce3333df8d5c352516a5f7afcc87b
Dominik Dorfmeister <office@dorfmeister.cc>
2024-06-23 17:29:52 +0200

    about

--- src/@lekoarts/gatsby-theme-minimal-blog/texts/About.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/texts/About.tsx
@@ -1,75 +1,139 @@
+import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
-import { Text, Link, Box } from 'theme-ui'
+import { Link, Box } from 'theme-ui'
 
 const About = () => (
-  <Box sx={{ display: ['block', 'block', 'flex'] }}>
-    <img
-      src="https://avatars.githubusercontent.com/u/1021430?s=128"
-      alt="TkDodo"
-      style={{
-        marginRight: '0.875rem',
-        marginTop: '0.5rem',
-        width: '8rem',
-        height: '8rem',
-        borderRadius: '50%',
+  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+    <Box
+      as="p"
+      sx={{
+        display: ['block', 'block', 'flex'],
+        gap: 4,
       }}
-    />
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <p style={{ marginBottom: 0, marginTop: 0 }}>
-        <Text sx={{ fontWeight: 'bold' }}>
-          Hi 👋, I'm Dominik from Vienna 🇦🇹
-        </Text>
-      </p>
-      <p style={{ marginBottom: 0 }}>
-        <Text>
-          I'm a Web Developer with a passion for ReactJs. I ❤️ static
-          types, especially TypeScript.
-        </Text>
-      </p>
-      <p style={{ marginBottom: 0 }}>
-        <Text>
-          I currently co-maintain the popular async state management
-          library{' '}
+    >
+      <Box
+        sx={{
+          width: ['10rem', null, '19rem'],
+          marginBottom: [null, null, 5],
+        }}
+      >
+        <StaticImage
+          alt="TkDodo"
+          style={{
+            borderRadius: '50%',
+          }}
+          aspectRatio="constrained"
+          src="../../../../static/images/profile.jpg"
+        />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          maxWidth: ['100%', null, '25rem'],
+        }}
+      >
+        <h1> Hi 👋, I'm Dominik from Vienna 🇦🇹</h1>
+        <p>
+          I'm a Web Developer and open source maintainer who ❤️
+          ReactJs and TypeScript. I'm currently maintaining{' '}
           <Link
             href="https://github.com/TanStack/query"
             target="_blank"
             rel="noreferrer noopener"
           >
             TanStack/query
-          </Link>
-          , as well as{' '}
+          </Link>{' '}
+          and{' '}
           <Link
-            href="https://github.com/remeda/remeda"
+            href="https://github.com/TanStack/router"
             target="_blank"
             rel="noreferrer noopener"
           >
-            remeda
+            TanStack/router
           </Link>
-          , a utility library tailored to TypeScript users.
-        </Text>
-      </p>
-      <p style={{ marginBottom: 0 }}>
-        <Text>
-          Welcome to my personal blog 📚, where I write about all things
-          React, TypeScript and of course react-query.
-        </Text>
-      </p>
-      <p style={{ marginBottom: 0 }}>
-        <Text>
-          If you enjoy my blog posts or want to support my open source
-          work, you can{' '}
-          <Link
-            href="https://github.com/sponsors/TkDodo"
-            target="_blank"
-            rel="noopener noreferrer"
-            sx={{ whiteSpace: 'nowrap' }}
-          >
-            🎗 sponsor me on Github 🎗
-          </Link>
-        </Text>
-      </p>
-    </div>
+          .
+        </p>
+      </Box>
+    </Box>
+    <p>
+      Welcome to my personal blog 📚, where I write about all things
+      React, TypeScript and of course the{' '}
+      <Link
+        href="https://github.com/TanStack"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        TanStack
+      </Link>
+      . If you enjoy my blog posts or want to support my open source
+      work, you can{' '}
+      <Link
+        href="https://github.com/sponsors/TkDodo"
+        target="_blank"
+        rel="noopener noreferrer"
+        sx={{ whiteSpace: 'nowrap' }}
+      >
+        🎗 sponsor me on Github 🎗
+      </Link>
+    </p>
   </Box>
 )
+
+/**
+ * <div style={{ display: 'flex', flexDirection: 'column' }}>
+ *       <p style={{ marginBottom: 0, marginTop: 0 }}>
+ *         <Text sx={{ fontWeight: 'bold' }}></Text>
+ *       </p>
+ *       <p style={{ marginBottom: 0 }}>
+ *         <Text>
+ *           I'm a Web Developer with a passion for ReactJs. I ❤️ static
+ *
+ *         </Text>
+ *       </p>
+ *       <p style={{ marginBottom: 0 }}>
+ *         <Text>
+ *           I currently co-maintain the popular async state management
+ *           library{' '}
+            <Link
+              href="https://github.com/TanStack/query"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              TanStack/query
+            </Link>
+ *           , as well as{' '}
+ *           <Link
+ *             href="https://github.com/remeda/remeda"
+ *             target="_blank"
+ *             rel="noreferrer noopener"
+ *           >
+ *             remeda
+ *           </Link>
+ *           , a utility library tailored to TypeScript users.
+ *         </Text>
+ *       </p>
+ *       <p style={{ marginBottom: 0 }}>
+ *         <Text>
+ *           Welcome to my personal blog 📚, where I write about all
+ *           things React, TypeScript and of course react-query.
+ *         </Text>
+ *       </p>
+ *       <p style={{ marginBottom: 0 }}>
+ *         <Text>
+ *           If you enjoy my blog posts or want to support my open source
+ *           work, you can{' '}
+ *           <Link
+ *             href="https://github.com/sponsors/TkDodo"
+ *             target="_blank"
+ *             rel="noopener noreferrer"
+ *             sx={{ whiteSpace: 'nowrap' }}
+ *           >
+ *             🎗 sponsor me on Github 🎗
+ *           </Link>
+ *         </Text>
+ *       </p>
+ *     </div>
+ */
 
 export default About
