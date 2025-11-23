a52301b0a91a1619262c30d82be63d4bdb63b8b1
TkDodo <office@dorfmeister.cc>
2025-11-23 11:16:51 +0100

    fix: light-mode flash

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,35 +1,5 @@
-import { jsx } from 'theme-ui'
 import * as React from 'react'
 import { withPrefix } from 'gatsby'
-
-const noFlashDark = `(function () {
-  try {
-    var hasLocalStorage = localStorage.getItem('theme-ui-color-mode');
-
-    if (
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    ) {
-      document.querySelector('html').setAttribute('data-theme', 'dark')
-      if (!hasLocalStorage) {
-        document.documentElement.classList.add('theme-ui-dark')
-        window.addEventListener('load', () => {
-          document.documentElement.classList.remove('theme-ui-dark')
-        });
-      }
-    }
-  } catch (err) {}
-})();`
-
-export const onRenderBody = ({ setPreBodyComponents }) => {
-  setPreBodyComponents([
-    jsx('script', {
-      key: 'theme-ui-no-flash-dark',
-      dangerouslySetInnerHTML: {
-        __html: noFlashDark,
-      },
-    }),
-  ])
-}
 
 const fontUrl = `${withPrefix('fonts/Inter.woff2')}`
 const font = `@font-face{font-family:'Inter';font-style:normal;font-weight:100;font-display:block;src:url(${fontUrl}) format('woff2');}@font-face{font-family:'Inter';font-style:normal;font-weight:200;font-display:block;src:url(${fontUrl}) format('woff2');}@font-face{font-family:'Inter';font-style:normal;font-weight:300;font-display:block;src:url(${fontUrl}) format('woff2');}@font-face{font-family:'Inter';font-style:normal;font-weight:400;font-display:block;src:url(${fontUrl}) format('woff2');}@font-face{font-family:'Inter';font-style:normal;font-weight:500;font-display:block;src:url(${fontUrl}) format('woff2');}@font-face{font-family:'Inter';font-style:normal;font-weight:600;font-display:block;src:url(${fontUrl}) format('woff2');}@font-face{font-family:'Inter';font-style:normal;font-weight:700;font-display:block;src:url(${fontUrl}) format('woff2');}@font-face{font-family:'Inter';font-style:normal;font-weight:800;font-display:block;src:url(${fontUrl}) format('woff2');}@font-face{font-family:'Inter';font-style:normal;font-weight:900;font-display:block;src:url(${fontUrl}) format('woff2');}`
