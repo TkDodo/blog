d180570fa6dfaf3e614a72974459ffcb5dd1c980
TkDodo <office@dorfmeister.cc>
2026-02-15 15:47:29 +0100

    better TsError

--- src/content-components/TsError.tsx
+++ src/content-components/TsError.tsx
@@ -6,8 +6,10 @@ type Props = {
 
 export function TsError({ children }: Props) {
   return (
-    <pre className="text-[#b00020] whitespace-pre-wrap">
-      <code>{children}</code>
-    </pre>
+    <aside className="not-prose my-5 overflow-hidden rounded-lg border-l-[6px] border-danger bg-border p-4 text-text dark:border-red-300 dark:bg-red-950/25">
+      <code className="block whitespace-pre-wrap font-mono text-[0.9em] leading-relaxed text-danger dark:text-red-200">
+        {children}
+      </code>
+    </aside>
   );
 }
