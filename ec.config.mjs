f9c3907dd66fd54d4fc95faf3f2230e369ca4493
TkDodo <office@dorfmeister.cc>
2026-02-16 11:42:04 +0100

    night-owl-light

--- ec.config.mjs
+++ ec.config.mjs
@@ -3,8 +3,11 @@ import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import { defineEcConfig } from "astro-expressive-code";
 
 export default defineEcConfig({
-  themes: ["night-owl"],
+  themes: ['night-owl', 'night-owl-light'],
+  useDarkModeMediaQuery: false,
+  themeCssSelector: (theme) => `[data-theme='${theme.type}']`,
   plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
+  removeUnusedThemes: true,
   defaultProps: {
     showLineNumbers: false,
     collapseStyle: "collapsible-auto",
