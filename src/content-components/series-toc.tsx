29d69093757ea2938b58a5fdb8769f8151d5ad55
honey32 <honey32@bearwapps.com>
2026-02-26 12:11:53 +0900

    refactor(SeriesToc): preprocess items array

--- src/content-components/series-toc.tsx
+++ src/content-components/series-toc.tsx
@@ -3,6 +3,18 @@ interface SeriesItem {
   title: string;
 }
 
+/**
+ * for internal logic only.
+ */
+interface ItemViewModel extends SeriesItem {
+  /**
+   * - positive: later (especially, 1 means the next one)
+   * - zero: current
+   * - negative: earlier (especially, -1 means the previous one)
+   */
+  diff: number;
+}
+
 interface Props {
   id?: string;
   items: ReadonlyArray<SeriesItem>;
@@ -11,11 +23,18 @@ interface Props {
 export default function SeriesToc({ id, items }: Props) {
   if (!id) return null;
 
+  const currentIndex = items.findIndex((item) => item.id === id);
+  if (currentIndex === -1) return null;
+
+  const itemsAsViewModel = items.map(
+    (item, i): ItemViewModel => ({ ...item, diff: i - currentIndex }),
+  );
+
   return (
     <div className="rounded-lg bg-(--color-ic-bg) p-4">
       <ul className="!my-2 flex list-none flex-col gap-[0.125rem] !p-0">
-        {items.map((item) => {
-          if (item.id === id) {
+        {itemsAsViewModel.map((item) => {
+          if (item.diff === 0) {
             return (
               <li key={item.id} className="!m-0">
                 <span className="block leading-relaxed font-bold">
