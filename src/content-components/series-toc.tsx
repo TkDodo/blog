ad2d0c9023210f06eac5ca2e8d517e1e5b46ccff
honey32 <honey32@bearwapps.com>
2026-02-26 11:56:31 +0900

    feat(SeriesToc): turn to a sheet appearance

--- src/content-components/series-toc.tsx
+++ src/content-components/series-toc.tsx
@@ -12,26 +12,28 @@ export default function SeriesToc({ id, items }: Props) {
   if (!id) return null;
 
   return (
-    <ul className="m-0 flex list-none flex-col gap-[0.125rem] pt-1 pb-2 pl-0">
-      {items.map((item) => {
-        if (item.id === id) {
+    <div className="rounded-lg bg-(--color-ic-bg) p-4">
+      <ul className="!my-2 flex list-none flex-col gap-[0.125rem] !p-0">
+        {items.map((item) => {
+          if (item.id === id) {
+            return (
+              <li key={item.id} className="!m-0">
+                <span className="block leading-relaxed font-bold">
+                  {item.title}
+                </span>
+              </li>
+            );
+          }
+
           return (
             <li key={item.id} className="!m-0">
-              <span className="block leading-relaxed font-bold">
-                {item.title}
+              <span className="block leading-relaxed">
+                <a href={`./${item.id}`}>{item.title}</a>
               </span>
             </li>
           );
-        }
-
-        return (
-          <li key={item.id} className="!m-0">
-            <span className="block leading-relaxed">
-              <a href={`./${item.id}`}>{item.title}</a>
-            </span>
-          </li>
-        );
-      })}
-    </ul>
+        })}
+      </ul>
+    </div>
   );
 }
