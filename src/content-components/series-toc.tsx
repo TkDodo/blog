23ab5246a3b4c70433484a459c8748e879bb9642
honey32 <honey32@bearwapps.com>
2026-02-26 18:11:36 +0900

    docs(SeriesToc): add description of `diff`

--- src/content-components/series-toc.tsx
+++ src/content-components/series-toc.tsx
@@ -82,6 +82,8 @@ export default function SeriesToc({ id, items }: Props) {
  */
 interface ItemViewModel extends SeriesItem {
   /**
+   * difference in the formula: `{this item's index} - {current item's index}`.
+   * 
    * - positive: later (especially, 1 means the next one)
    * - zero: current
    * - negative: earlier (especially, -1 means the previous one)
